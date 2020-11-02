79b1de2dcb1eaa58ce21d32d3608b01448c14408
nayan-bansal <nayan.7991@gmail.com>
2020-11-02 22:50:43 +0530

    Added: Update Salary in the Database

--- Employee_Payroll_Service_Test.java
+++ Employee_Payroll_Service_Test.java
@@ -9,35 +9,41 @@ import org.junit.Test;
 import main.java.Employee.Employee_Payroll.EmployeePayrollData;
 import main.java.Employee.Employee_Payroll.EmployeePayrollService;
 import main.java.Employee.Employee_Payroll.EmployeePayrollService.IOService;
+import main.java.Employee.Employee_Payroll.PayrollSystemException;
 
 public class Employee_Payroll_Service_Test {
 
-	
 	@Test
 	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEnteries() {
-		EmployeePayrollData[] arrayOfEmps= {
-				new EmployeePayrollData(1,"mark", 50000.0),
-				new EmployeePayrollData(2,"bill", 50000.0),
-				new EmployeePayrollData(3,"terisa", 200000.0)
-		};
+		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, "Diksha kalra", 100000.0),
+				new EmployeePayrollData(2, "Bill Gates", 200000.0),
+				new EmployeePayrollData(3, "mark Zuckerberg", 300000.0) };
 		EmployeePayrollService employeePayrollService;
-		employeePayrollService=new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
 		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
-		long entries=employeePayrollService.countEntries(IOService.FILE_IO);
-		Assert.assertEquals(3,entries);	
+		long entries = employeePayrollService.countEntries(IOService.FILE_IO);
+		Assert.assertEquals(3, entries);
 	}
-	
+
 	@Test
 	public void givenFileOnReadingFileShouldMatchEmployeeCount() {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-		List<EmployeePayrollData> entries = employeePayrollService.readEmployeePayrollData(IOService.FILE_IO);
+		List<EmployeePayrollData> entries = employeePayrollService.readPayrollData(IOService.FILE_IO);
+	}
+
+	@Test
+	public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+		Assert.assertEquals(3, employeePayrollData.size());
 	}
 	
 	@Test
-	public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
-	EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-	List<EmployeePayrollData> employeePayrollData=employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
-	Assert.assertEquals(3,employeePayrollData.size());
+	public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB() throws PayrollSystemException {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+		employeePayrollService.updateEmployeeSalary("terisa",3000000.0);
+		boolean result=employeePayrollService.checkEmployeePayrollInSyncWithDB("terisa");
+		Assert.assertTrue(result);
 	}
-	
 }
