23d2b022c66ebbe588213669a9ba64353f4949da
nayan-bansal <nayan.7991@gmail.com>
2020-11-02 17:20:18 +0530

    Added: getter setter file for database

--- EmployeePayrollData.java
+++ EmployeePayrollData.java
@@ -1,18 +1,30 @@
 package main.java.Employee.Employee_Payroll;
 
 public class EmployeePayrollData {
-	
+
 	public int id;
 	public String name;
 	public double salary;
 
-	public EmployeePayrollData(Integer id, String name, Double salary) {
+	public EmployeePayrollData(int id, String name, double salary) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.salary = salary;
 	}
 
+	@Override
 	public String toString() {
-		return "id= " + id + ", name= " + name + '\'' + ", salary= " + salary;
+		return "Employee Id: " + id + " Employee Name: " + name + " Employee Salary: " + salary;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this==o) return true;
+		if(o==null || getClass()!=o.getClass()) return false;
+		EmployeePayrollData that=(EmployeePayrollData) o;
+		return id==that.id  &&
+				Double.compare(that.salary, salary)==0  &&
+				name.equals(that.name);	
 	}
 }
