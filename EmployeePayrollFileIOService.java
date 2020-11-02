44c04212bd92c8db63244441e82e1b5a1a9055cc
nayan-bansal <nayan.7991@gmail.com>
2020-11-02 12:08:23 +0530

    Added: Ability to count and print employee Payroll

--- EmployeePayrollFileIOService.java
+++ EmployeePayrollFileIOService.java
@@ -1,9 +1,11 @@
 package main.java.Employee.Employee_Payroll;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+
 
 public class EmployeePayrollFileIOService {
 
@@ -21,4 +23,23 @@ public static String PAYROLL_FILE_NAME="payroll-file.txt";
 			e.printStackTrace();
 		}
 	}
+	
+	public void printData() {
+		try {
+			Files.lines(new File(PAYROLL_FILE_NAME).toPath()).forEach(System.out::println);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public long countEntries() {
+		long entries = 0;
+		try {
+			entries = Files.lines(new File(PAYROLL_FILE_NAME).toPath()).count();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return entries;
+	}
+	
 }
