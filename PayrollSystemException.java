7cea88168b5e38717aa0c2b4f1e69d8dba5b19c8
nayan-bansal <nayan.7991@gmail.com>
2020-11-02 22:54:51 +0530

    Added: Update Salary in the Database

---
+++ PayrollSystemException.java
@@ -0,0 +1,18 @@
+package main.java.Employee.Employee_Payroll;
+
+public class PayrollSystemException extends Exception{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	enum ExceptionType {
+		UPDATE_FILE_EXCEPTION
+	}
+
+	ExceptionType type;
+
+	public PayrollSystemException(String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+}
