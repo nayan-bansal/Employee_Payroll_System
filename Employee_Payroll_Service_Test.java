31a9f0e9aa78a75eafc368d81c580cedb23b4d55
nayan-bansal <69717938+nayan-bansal@users.noreply.github.com>
2020-11-03 03:20:37 +0530

    Update Employee_Payroll_Service_Test.java

--- Employee_Payroll_Service_Test.java
+++ Employee_Payroll_Service_Test.java
@@ -16,9 +16,9 @@ public class Employee_Payroll_Service_Test {
 
 	@Test
 	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEnteries() {
-		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, "Diksha kalra", 100000.0),
-				new EmployeePayrollData(2, "Bill Gates", 200000.0),
-				new EmployeePayrollData(3, "mark Zuckerberg", 300000.0) };
+		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, "mark", 100000.0),
+				new EmployeePayrollData(2, "bill", 200000.0),
+				new EmployeePayrollData(3, "terisa", 300000.0) };
 		EmployeePayrollService employeePayrollService;
 		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
 		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
